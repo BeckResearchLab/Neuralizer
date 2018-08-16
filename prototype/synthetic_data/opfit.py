6ba98dae343c42c9b494a23060eb6f61c383a73d
王悦凝 <wangyuening@ouetsugBookpuro.dhcp4.washington.edu>
2018-08-15 22:24:20 -0700

    multi_search modified

--- prototype/synthetic_data/opfit.py
+++ prototype/synthetic_data/opfit.py
@@ -108,46 +108,45 @@ def model_search(X_train,Y_train,X_test,Y_test
     return best_activation
 
  
-def model__multi_search(X_train,Y_train,X_test,Y_test,input_dim,output_dim,layers,
-                        activation_functions=['tanh', 'softmax', 'relu'],Dropout=[0.1,0.5,0.8]):
-    iterations = (len(Dropout)*len(activation_functions))**(layers+2)
-    inner_iterations = (len(Dropout)*len(activation_functions))**layers
-    options= make_combo(option1=activation_functions,option2=Dropout)
+def model_multi_search(X_train,Y_train,X_test,Y_test,input_dim,output_dim,layers,
+                        activation_functions=['tanh', 'softmax', 'relu'],units=[5,10,20]):
+    iterations = (len(units)*len(activation_functions))**(layers+1)*len(activation_functions)
+    inner_iterations = (len(units)*len(activation_functions))**layers
+    options= make_combo(option1=activation_functions,option2=units)
     af_combs = make_pairwise_list(max_depth=layers, options=options)
     print(f'{layers}\t{activation_functions}\t{iterations} iterations required')
     best_R = 0.0
     best_param = []
     iteration_n = 1
     for n in range(layers):
-        best_activation.append('none')
+        best_param.append(['none','none'])
     best_activation = []
     for inner_iteration in range(inner_iterations):
         for option_in in options:
             for layer in range(layers):
                 inner_list = []
-                inner_list.append(af_combs[inner_iteration])
-                for option_out in options:
+                inner_list.append(af_combs[inner_iteration][layer])
+                for activation_out in activation_functions:
                     print(f"running iteration {iteration_n}")
-                    parameter_list1 = []
+                    parameter_list = []
                     parameter_list.append(option_in)
                     parameter_list.extend(inner_list)
-                    parameter_list.append(option_out)
-                    print(f"create input layer with activation of {option_in[0]} and Dropout of {option_in[1]}")
+                    parameter_list.append(activation_out)
+                    print(f"create input layer with activation of {option_in[0]} and units of {option_in[1]}")
                     
                     model = keras.Sequential()
-                    model.add(keras.layers.Dense(10,input_dim = input_dim,activation=option_in[0],
-                               Dropout = option_in[1]                 ))
+                    model.add(keras.layers.Dense(option_in[1],input_dim = input_dim,activation=option_in[0]))
                     for i in range(len(inner_list)):
-                        print(f"create hidden layer {layer} of activation {inner_list[i][0]} and Dropout {inner_list[i][1]}")
-                        model.add(keras.layers.Dense(20,activation = inner_list[i][0],Dropout = inner_list[i][1]))
-                    print(f"create output layer with activation of {option_out[0]} and Dropout of {option_out[1]}")
-                    model.add(keras.layers.Dense(output_dim,activation=option_out[0],Dropout=option_out[1]))
+                        print(f"create hidden layer {layer} of activation {inner_list[i][0]} and units {inner_list[i][1]}")
+                        model.add(keras.layers.Dense(inner_list[i][1],activation = inner_list[i][0]))
+                    print(f"create output layer with activation of {activation_out} and units of {output_dim}")
+                    model.add(keras.layers.Dense(output_dim,activation=activation_out))
                     model.compile(loss='mean_squared_error', optimizer='adam', 
                                   metrics=[R_squared])
                     history = model.fit(X_train,Y_train,epochs=50, batch_size=10,validation_split=0.33)
                     scores = model.evaluate(X_test,Y_test)
                     
-                    iteration_n += iteration_n 
+                    iteration_n += 1 
                     if scores[1]>best_R:
                         best_param = parameter_list
                         best_R = scores[1]
@@ -156,8 +155,7 @@ def model__multi_search(X_train,Y_train,X_test,Y_test,input_dim,output_dim,layer
         print("")
     print(best_param)
     print(best_R)
-    return best_param
-    
+    return best_param    
 def layer_search(hidden_layers=[1,3,5],X_train,Y_train,X_test,Y_test
 		 input_dim,output_dims
  		 activation_functions,lr):
