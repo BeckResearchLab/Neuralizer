8b9e01b77f5fdbd6644f134ecc2605856ab9687f
王悦凝 <wangyuening@ouetsugBookpuro.dhcp4.washington.edu>
2018-09-06 17:11:01 -0700

    checkpoint added

--- prototype/synthetic_data/opfit.py
+++ prototype/synthetic_data/opfit.py
@@ -171,6 +171,74 @@ def model_multi_search(X_train,Y_train,X_test,Y_test,input_dim,output_dim,layers
     print('model took %0.2f seconds to train'%(end-start))
     return best_param,best_R
 
+def model_multi_search_new(X_train,Y_train,X_test,Y_test,input_dim,output_dim,layers,
+                        activation_functions=['tanh', 'softmax', 'relu'],units=[5,10,20]):
+    iterations = (len(units)*len(activation_functions))**(layers+1)*len(activation_functions)
+    inner_iterations = (len(units)*len(activation_functions))**layers
+    options= make_combo(option1=activation_functions,option2=units)
+    af_combs = make_pairwise_list(max_depth=layers, options=options)
+    print(f'{layers}\t{activation_functions}\t{iterations} iterations required')
+    best_R = 0.0
+    best_param = []
+    iteration_n = 1
+    cumulative_time = 0.0
+    for n in range(layers):
+        best_param.append(['none','none'])
+    best_activation = []
+    for inner_iteration in range(inner_iterations):
+        for option_in in options:
+            inner_list=[]
+            for k in range(layers):
+                inner_list.append(af_combs[inner_iteration][k])
+            for layer in range(layers):
+                for activation_out in activation_functions:
+                    print(inner_list)
+                    print(f"running iteration {iteration_n}")
+                    parameter_list = []
+                    parameter_list.append(option_in)
+                    parameter_list.extend(inner_list)
+                    parameter_list.append(activation_out)
+                    print(f"create input layer with activation of {option_in[0]} and units of {option_in[1]}")
+                    model = keras.Sequential()
+                    model.add(keras.layers.Dense(option_in[1],input_dim = input_dim,activation=option_in[0]))
+                    for i in range(len(inner_list)):
+                        print(f"create hidden layer {i+1} of activation {inner_list[i][0]} and units {inner_list[i][1]}")
+                        model.add(keras.layers.BatchNormalization(momentum=0.9))
+                        model.add(keras.layers.Dense(inner_list[i][1],activation = inner_list[i][0]))
+                    print(f"create output layer with activation of {activation_out} and units of {output_dim}")
+                    model.add(keras.layers.Dense(output_dim,activation=activation_out))
+                    model.compile(loss='mean_squared_error', optimizer='adam',
+                                  metrics=[R_squared])
+                    earlystop = keras.callbacks.EarlyStopping(monitor='val_R_squared',min_delta=0.0001,patience=20,mode='auto')
+                    output_folder = './intermediate_output'
+                    if not os.path.exists(output_folder):
+                        os.makedirs(ouput_folder)
+                    filepath=outputFolder+"/weights-{epoch:02d}-{val_R_squared:.2f}.hdf5"
+                    checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_R_squared', verbose=1, save_best_only=False, save_weights_only=True, mode='auto', period=10)
+                    callbacks_list = [earlystop,checkpoint]
+                    start = time.time()
+                    history = model.fit(X_train,Y_train,epochs=300, batch_size=10,callbacks=callbacks_list,validation_split=0.2,verbose=0)
+                    end = time.time()
+                    cumulative_time += (end-start)
+                    scores = model.evaluate(X_test,Y_test,verbose=0)
+                    iteration_n += 1
+                    if not os.path.exits("Results%d.txt"%(layers)):
+                        f = open("Results%d.txt"%(layers),"w+")
+                    else:
+                        f = open(Results%d.txt"%(layers),"a+")
+                    f.write("For this combination %s, R is %0.2f\r\n" %(parameter_list,scores[1]))
+                    if scores[1]>best_R:
+                        best_param = parameter_list
+                        best_R = scores[1]
+                    else:
+                        pass
+        print("")
+    f.close()
+    print(best_param)
+    print(best_R)
+    print('model took %0.2f seconds to train'%(cumulative_time))
+    return best_param,best_R
+
 def layer_search(X_train,Y_train,X_test,Y_test,
                  input_dim,output_dims,
                  activation_functions,units,hidden_layers=[1,3,5]):
