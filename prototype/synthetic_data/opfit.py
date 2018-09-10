1d5c858264b2e88acbec0aa9e50db90b59fbaef0
Yueningwang <yw246@uw.edu>
2018-09-10 14:21:31 -0700

    delete close file line

--- prototype/synthetic_data/opfit.py
+++ prototype/synthetic_data/opfit.py
@@ -345,7 +345,7 @@ def continue_model_search(epoch_num,starting_n,X_train,Y_train,X_test,Y_test,inp
                             best_param,best_R = model_creation_run_three(model,X_test,Y_test,iteration_n,parameter_list,layers,best_R,best_param)
                             run_once = 1
                             print("")
-    f.close()
+    
     print(best_param)
     print(best_R)
     print('model took %0.2f seconds to train'%(cumulative_time))
