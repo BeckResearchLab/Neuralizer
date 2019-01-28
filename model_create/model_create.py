29ddcaad8ee714fa09d090634543b2e3231ed448
Yueningwang <yw246@uw.edu>
2019-01-27 21:33:29 -0800

    many changes

--- model_create/model_create.py
+++ model_create/model_create.py
@@ -194,3 +194,59 @@ def model_continue_search(data,test_fraction,random_state,cumulative_time,params
                         iteration_n += 1
                         pass
                     else:
+                        parameter_list = []
+                        parameter_list.append(option_in)
+                        parameter_list.extend(inner_list)
+                        parameter_list.append(activation_out)
+                        print(f"create input layer with activation of {option_in[0]} and units of {option_in[1]}")
+                        model = keras.Sequential()
+                        model.add(keras.layers.Dense(option_in[1],input_dim = input_dim,activation=option_in[0]))
+                        for i in range(len(inner_list)):
+                            print(f"create hidden layer {i+1} of activation {inner_list[i][0]} and units {inner_list[i][1]}")
+                            model.add(keras.layers.BatchNormalization(momentum=0.9))
+                            model.add(keras.layers.Dense(inner_list[i][1],activation = inner_list[i][0]))
+                        print(f"create output layer with activation of {activation_out} and units of {output_dim}")
+                        model.add(keras.layers.Dense(output_dim,activation=activation_out))
+                        model.compile(loss='mean_squared_error', optimizer='adam',metrics=[R_squared])
+                        earlystop = keras.callbacks.EarlyStopping(monitor='val_R_squared',min_delta=0.0001,patience=20,mode='auto')
+                        output_folder = './collection%d/intermediate_output%d' % (layers,iteration_n)
+                        file_ini = output_folder+'/weights-'+str(epoch_num)+'*'
+                        filename = glob.glob(file_ini)
+                        print(filename)
+                        if os.path.isfile(filename[0]):
+                            model.load_weights(filename[0])
+                        else:
+                            print("%s does not exists" % (filename[0]))
+                        collection_folder = './collection%d' % (layers)
+                        if not os.path.exists(collection_folder):
+                            os.makedirs(collection_folder)
+                        output_folder = './collection%d/intermediate_output%d'%(layers,iteration_n)
+                        if not os.path.exists(output_folder):
+                            os.makedirs(output_folder)
+                        filepath=output_folder+"/weights-{epoch:02d}-{val_R_squared:.2f}.hdf5"
+                        checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_R_squared', verbose=1, save_best_only=False, save_weights_only=True, mode='auto', period=10)
+                        callbacks_list = [earlystop,checkpoint]
+                        start = time.time()
+                        history = model.fit(X_train,Y_train,epochs=300,batch_size=10,callbacks=callbacks_list,validation_split=0.2,initial_epoch=epoch_num+1,verbose=0)
+                        end = time.time()
+                        cumulative_time += (end-start)
+                        print('it already took %0.2f seconds' % (cumulative_time))
+                        scores = model.evaluate(X_test,Y_test,verbose=0)
+                        iteration_n += 1
+                        if not os.path.exists("Results%d.txt"%(layers)):
+                            f = open("Results%d.txt"%(layers),"w+")
+                        else:
+                            f = open("Results%d.txt"%(layers),"a+")
+                        f.write("For this combination %s, R is %0.2f\r\n" %(parameter_list,scores[1]))
+                        if scores[1]>best_R:
+                            best_param = parameter_list
+                            best_R = scores[1]
+                        else:
+                            pass
+                        f.write("The best_R for now is %0.4f and combination is %s "% (best_R,best_param))
+                        run_once = 1
+                        iteration_n += 1
+                        x ={"layer_number":layers,"starting_n":iteration_n-1,"best_R":best_R,"best_param":best_param,"cumulative_time":cumulative_time}
+                        print(x)
+                        check_write(x,'latest.json')
+                        print("")
