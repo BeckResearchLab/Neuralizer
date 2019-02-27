c3bf97892b68d3a760a713038ab593130f26390f
Yueningwang <yw246@uw.edu>
2019-02-26 22:31:02 -0800

    test functions for data process modified

--- neuralizer/tests/test_data_process.py
+++ neuralizer/tests/test_data_process.py
@@ -2,7 +2,14 @@ import os
 import pandas as pd
 import numpy as np
 import data_process as dp
-from data_process import *
 
 def test_read_file():
+    X,Y = dp.read_file('test.tsv',["A","B"],"y")
+    assert (X == [[0,1],[3,2],[4,3]]),"The data of predictors is loaded wrong"
+    assert (Y == [0,1,5]),"data of Y has problems"
 
+def test_data)info():
+    data = {"filename":"test.tsv","X_var":["A","B"],"Y_var":"y"}
+    X,Y,input_dim,output_dim = dp.data_info(data)
+    assert(input_dim == 2),"Dimension of input layer is not correct"
+    assert(output_dim == 1), "Dimension of output layer is not correct"
