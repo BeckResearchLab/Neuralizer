a228f430e98653a47b9de73771cd0a65553a8301
Yueningwang <yw246@uw.edu>
2019-02-26 23:00:27 -0800

    __init__.py modified

--- neuralizer/tests/test_data_process.py
+++ neuralizer/tests/test_data_process.py
@@ -1,14 +1,16 @@
+from __future__ import absolute_import,division,print_function
 import os
 import pandas as pd
 import numpy as np
+import numpy.testing as npt
 import data_process as dp
 
 def test_read_file():
     X,Y = dp.read_file('test.tsv',["A","B"],"y")
-    assert (X == [[0,1],[3,2],[4,3]]),"The data of predictors is loaded wrong"
-    assert (Y == [0,1,5]),"data of Y has problems"
+    npt.assert_equal(X,np.array[[0,1],[3,2],[4,3]])
+    npt.assert_equal(Y,np.array[0,1,5])
 
-def test_data)info():
+def test_data_info():
     data = {"filename":"test.tsv","X_var":["A","B"],"Y_var":"y"}
     X,Y,input_dim,output_dim = dp.data_info(data)
     assert(input_dim == 2),"Dimension of input layer is not correct"
