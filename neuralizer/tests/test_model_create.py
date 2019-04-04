1e0bd5820f3827fd54ad960e731e0000de131a39
王悦凝 <wangyuening@D-10-154-15-153.dhcp4.washington.edu>
2019-04-03 23:03:06 -0700

    More test added

--- neuralizer/tests/test_model_create.py
+++ neuralizer/tests/test_model_create.py
@@ -1,4 +1,17 @@
-import model_create
+import model_create as mc
 
-def test_make_combo:
+def test_make_combo():
+	parameter_combo = mc.make_combo()
+	assert len(parameter_combo) == 9; "The combination of hyperparameter choices is not correct"
 
+def test_make_pairwise_list():
+	max_depth =2
+	options = [1,5,10]
+	combinations = mc.make_pairwise_list(max_depth=max_depth, options=options)
+	length = len(options)**max_depth
+	
+	assert len(combinations) == length,'not every situation is considered'
+
+	assert combinations[0][1] == 1, 'The hyperparameter is not as chosen'
+	assert combinations[5][0] == 5, 'The hyperparameters are not combined as designed'
+	assert combinations[2][1] == 10, 'The hyperparameters are not combined as designed'
